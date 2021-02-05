0fcf4781333a8f244ef11f3b15e5025332ce50d1
Duc Pham <ducpmth1903004@fpt.edu.vn>
2021-02-06 04:21:09 +0700

    update_users

--- MadamSolution.ViewModels/System/Users/UserVm.cs
+++ MadamSolution.ViewModels/System/Users/UserVm.cs
@@ -16,5 +16,7 @@ namespace MadamSolution.ViewModels.System.Users
         public string UserName { get; set; }
 
         public string Email { get; set; }
+
+        public DateTime Dob { get; set; }
     }
 }
